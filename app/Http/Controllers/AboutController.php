b79bcc3eb922081114e96e9310cb0dff8a5b5124
maretyatya <maretyatianhar@gmail.com>
2022-09-06 23:55:29 +0700

    Prakikum 2 bagian 3

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -4,24 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class PageController extends Controller
+class AboutController extends Controller
 {
-    public function index()
-    {
-        echo "Selamat Datang";
-    }
 
-    public function about()
+    public function index()
     {
         echo "NIM : 2241727019";
         echo "<br>";
         echo "Nama : Maretyatianhar Gunsa";
         echo "<br>";
         echo "Kelas : TI-4J";
-    }
-
-    public function articles($id)
-    {
-        echo "Halaman Artikel dengan ID " . $id;
     }
 }
